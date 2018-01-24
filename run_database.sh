d71bb33d04437b535d4a81fa46dcaea722c10e02
Masayuki Igawa <masayuki@igawa.io>
2018-01-24 17:10:58 +0900

    Update db name

--- run_database.sh
+++ run_database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -p 3306:3306  --rm --name some-mariadb -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mariadb:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run -p 3306:3306  --rm --name openstack-health-mariadb -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mariadb:latest --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
